0de027feccf773b47c196405e709ba7c7fefb0b0
Mustafa Gonul <mustafagonul@gmail.com>
2017-12-09 23:54:04 +0300

    - API Boilerplate mustafagonul/Fall2017Swe573#18 - .travis directory removed

--- .travis/prepare-mongodb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sleep 15
-mongo githubnize-travis-ci --eval 'db.createCollection("GithubnizeUsers");'
-mongo githubnize-travis-ci --eval 'db.createCollection("GithubnizeGroups");'
-mongo githubnize-travis-ci --eval 'db.createUser({user:"githubnize-travis-ci",pwd:"123456", roles:[{role:"readWrite",db:"githubnize-travis-ci"}]});'
